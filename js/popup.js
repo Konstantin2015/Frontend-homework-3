a3fb75b5a5b12ba586dcc63a18046b914fce64a2
Konstantin2015 <Goldenkott@gmail.com>
2016-11-15 19:28:25 +0200

    initial commit of Frontend-homework-3 files

---
+++ js/popup.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Костя on 03.01.2017.
+ */
+$(document).ready(function() {
+    
+});
+    $('.btn-popup').on('click', function() {
+        $('.ops').show();
+});
+$('.no').click(function() {
+    $('.ops').hide();
+});
+$('.yes').click(function() {
+    $('.ops').hide();
+});
+
+
