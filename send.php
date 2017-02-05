d75b21e1da621f5ca9ad7c7538cf1f164e542e53
Konstantin2015 <Goldenkott@gmail.com>
2017-02-06 00:26:39 +0200

    initial

--- send.php
+++ send.php
@@ -3,10 +3,12 @@ $name = $_POST['name'];
 $surname = $_POST['surname'];
 $company =  $_POST['company'];
 $street =  $_POST['street'];
-$city =  $_POST['city'];
+$city =  $_POST['city'] ;
 $state =  $_POST['state'];
 $country =  $_POST['country'];
 $zipcode =  $_POST['zipcode'];
+$cntrinterest =  $_POST['cntrinterest'];
+$notes =  $_POST['notes'];
 
 
 $name = htmlspecialchars($name);
@@ -17,6 +19,8 @@ $city =  htmlspecialchars($city);
 $state =  htmlspecialchars($state);
 $country =  htmlspecialchars($country);
 $zipcode = htmlspecialchars($zipcode);
+$cntrinterest =  htmlspecialchars($cntrinterest);
+$notes =  htmlspecialchars($notes);
 
 $name = urldecode($name);
 $surname = urldecode($surname);
@@ -26,6 +30,8 @@ $city =  urldecode($city);
 $state =  urldecode($state);
 $country =  urldecode($country);
 $zipcode = urldecode($zipcode);
+$cntrinterest =  urldecode($cntrinterest);
+$notes =  urldecode($notes);
 
 $name = trim($name);
 $surname = trim($surname);
@@ -35,12 +41,22 @@ $city =  trim($city);
 $state =  trim($state);
 $country =  trim($country);
 $zipcode = trim($zipcode);
+$cntrinterest =  trim($cntrinterest);
+$notes =  trim($notes);
 
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("office@globalvision.online", "Форма обратной связи.", "First name:".$name.". Last name: ".$surname.".Company address: ".$company.". Street:".$street.".City:".$city.".State:".$state.".Country:".$country.".ZipCode:".$zipcode ,"From:lomakoira@mail.ru \r\n"))
-{     echo "сообщение успешно отправлено";
+$to  = "office@globalvision.online, lomakoira@mail.ru" ;
+
+
+if(isset($_POST['check']))
+    $check .= "\nNorth America";
+if(isset($_POST['check']))
+    $check .= "\nMiddle East";
+if(isset($_POST['check']))
+    $check .= "\nLatin America";
+
+
+if (mail("$to", "Форма обратной связи.", "\r\n First name:".$name.". \r\n Last name: ".$surname.". \r\n Company address: ".$company.". \r\n Street:".$street.". \r\n City:".$city.". \r\n State:".$state.". \r\n Country:".$country.". \r\n ZipCode:".$zipcode.". \r\n Region of Distribution Interest*:".$check.". \r\n Country of Interest:" .$cntrinterest.". \r\n Additional Notes:" .$notes,"From:TestTest@mail.com \r\n"))
+{     echo "Message sent successfully";
 } else {
-    echo "при отправке сообщения возникли ошибки";
+    echo "When you send any errors";
 }?>
